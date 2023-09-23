2797b92eb32abe545bfb6ac51e390fc7eebbff25
alexkozhura <alexkozhura@gmail.com>
2023-09-23 15:40:33 +0200

    Added config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7nWBih9kWKMForK4lIsRmZvkpz-zgnp4",
+    authDomain: "reading-list-app-77d8f.firebaseapp.com",
+    projectId: "reading-list-app-77d8f",
+    storageBucket: "reading-list-app-77d8f.appspot.com",
+    messagingSenderId: "822330714808",
+    appId: "1:822330714808:web:1472535f1eacf4f318224d"
+  };
