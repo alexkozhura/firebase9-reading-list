c7ed906fc89f305a0de5023dd6eef53396d15c34
alexkozhura <alexkozhura@gmail.com>
2023-09-23 15:56:02 +0200

    Added more Firebase configs

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyA7nWBih9kWKMForK4lIsRmZvkpz-zgnp4",
     authDomain: "reading-list-app-77d8f.firebaseapp.com",
@@ -6,3 +9,11 @@ const firebaseConfig = {
     messagingSenderId: "822330714808",
     appId: "1:822330714808:web:1472535f1eacf4f318224d"
   };
+
+// init Firebase
+initializeApp(firebaseConfig)
+
+// init Firestore
+const db = getFirestore()
+
+export { db } 
