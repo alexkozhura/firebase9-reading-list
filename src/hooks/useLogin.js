8d8aea9cfb81b2d48f16f01999f2dfeea3734086
alexkozhura <alexkozhura@gmail.com>
2023-09-25 19:15:06 +0200

    Added AuthContext and useAuthContext

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
-export const useLogin = () => {
+import { signInWithEmailAndPassword } from "firebase/auth"
 
+export const useLogin = () => {
+    const [error, setError] = useState(null)
+
+    const login = (auth, email, password) => {
+        setError(null)
+        signInWithEmailAndPassword(auth, email, password)
+            .then((res) => {
+                console.log('user logged in: ', res.user)
+            })
+            .catch((err) => {
+                setError(err.message)
+            })
+    }
+
+    return { error, login }
 }
